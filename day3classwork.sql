ed7ee7f91220be72b45ce8a9e4067619f5df26b6
Gayathri-12s <gowri7350@gmail.com>
2025-09-21 13:28:59 +0530

    Add SQL script for product data insertion and queries

---
+++ day3classwork.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO products_table1(name, category, price, in_stock) VALUES('Jeans', 'Cloth', '2499', 'No' ), 
+('bread', 'Groceries', '80', 'Yes'), 
+('Shoes', 'Footwear', '7499', 'Yes'),
+ ('Salt 5kg', 'Groceries', '120', 'Yes'),
+  ('Rice 1kg','Groceries','60', 'No');
+SELECT DISTINCT category FROM products_table1;
+SELECT * FROM products_table1 WHERE in_stock = 'Yes' AND price < 500;
+SELECT * FROM products_table1 WHERE in_stock = 'No' OR price > 1000;
+SELECT name, price FROM products_table1 ORDER BY price DESC;
+SELECT name, price*1.18 AS 'price_with_tax' FROM products_table1;
